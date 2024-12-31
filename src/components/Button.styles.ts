259371036fc4af3db2b42dfcadbfc917e19fc628
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-31 15:02:30 -0300

    feat: add app colors/font families

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,19 +6,12 @@ interface ButtonContainerProps {
 	variant: ButtonVariant;
 }
 
-// const buttonVariants = {
-// 	primary: 'purple',
-// 	secondary: 'orange',
-// 	danger: 'red',
-// 	success: 'green'
-// };
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
 	margin: 8px;
 	border: 0;
 	border-radius: 4px;
 	width: 100px;
 	height: 40px;
-	background-color: ${props => props.theme.primary};
+	background-color: ${props => props.theme['green-500']};
 	color: ${props => props.theme.white};
 `;
