aa99056c061e77cfe6c914f9133c1e8d2c6fe013
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-31 14:52:34 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+
+import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
 			<Button variant="primary" />
 			<Button variant="secondary" />
 			<Button variant="success" />
