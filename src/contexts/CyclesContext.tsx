53f9385bc22377308b30350b3ab88580f57d6a09
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-01-08 23:51:31 -0300

    fix: add function to be called after submitting the create new cycle form

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -64,7 +64,6 @@ function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 		setCycles(state => [...state, newCycle]);
 		setActiveCycleId(id);
 		setAmountSecondsPassed(0);
-		// reset();
 	}
 
 	function interruptCurrentCycle() {
