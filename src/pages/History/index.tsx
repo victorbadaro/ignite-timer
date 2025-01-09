28a51c8d2d90df240de2590136b677eb4bbfb548
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-01-09 11:40:59 -0300

    feat: add cycles list to history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,10 +1,10 @@
-// import { useContext } from 'react';
-// import { CyclesContext } from '@/contexts/CyclesContext';
+import { CyclesContext } from '@/contexts/CyclesContext';
+import { useContext } from 'react';
 
 import { HistoryContainer, HistoryList, Status } from './styles';
 
 export function History() {
-	// const { cycles } = useContext(CyclesContext);
+	const { cycles } = useContext(CyclesContext);
 
 	return (
 		<HistoryContainer>
@@ -21,62 +21,26 @@ export function History() {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="yellow">Em andamento</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="red">Interrompido</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
-						<tr>
-							<td>Tarefa</td>
-							<td>20 minutos</td>
-							<td>Há 2 meses</td>
-							<td>
-								<Status statusColor="green">Concluído</Status>
-							</td>
-						</tr>
+						{cycles.map(cycle => (
+							<tr key={cycle.id}>
+								<td>{cycle.task}</td>
+								<td>{cycle.minutesAmount} minutos</td>
+								<td>{cycle.startDate.toISOString()}</td>
+								<td>
+									{cycle.finishedDate ? (
+										<Status statusColor="green">Concluído</Status>
+									) : null}
+
+									{cycle.interruptedDate ? (
+										<Status statusColor="red">Interrompido</Status>
+									) : null}
+
+									{!cycle.finishedDate && !cycle.interruptedDate ? (
+										<Status statusColor="yellow">Em andamento</Status>
+									) : null}
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</HistoryList>
