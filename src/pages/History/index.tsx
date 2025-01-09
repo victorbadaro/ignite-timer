74c896b6aab8338b30451af23a359f8614de645b
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-01-09 11:46:40 -0300

    feat: improve history table ui to show cycles start date in a better way

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { CyclesContext } from '@/contexts/CyclesContext';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { useContext } from 'react';
 
 import { HistoryContainer, HistoryList, Status } from './styles';
@@ -25,7 +27,12 @@ export function History() {
 							<tr key={cycle.id}>
 								<td>{cycle.task}</td>
 								<td>{cycle.minutesAmount} minutos</td>
-								<td>{cycle.startDate.toISOString()}</td>
+								<td>
+									{formatDistanceToNow(cycle.startDate, {
+										addSuffix: true,
+										locale: ptBR
+									})}
+								</td>
 								<td>
 									{cycle.finishedDate ? (
 										<Status statusColor="green">Concluído</Status>
