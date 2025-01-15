fbbd22c897d454db1e9eede6c213e3e63e9eee04
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-01-14 23:27:23 -0300

    refactor: separate actions into a different file

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
-import { ActionTypes, Cycle, cyclesReducer } from '@/reducers/cycles';
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from '@/reducers/cycles/actions';
+import { Cycle, cyclesReducer } from '@/reducers/cycles/reducer';
 import { createContext, ReactNode, useReducer, useState } from 'react';
 
 interface CreateCycleData {
@@ -38,12 +39,7 @@ function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 	}
 
 	function markCurrentCycleAsFinished() {
-		dispatch({
-			type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-			payload: {
-				activeCycleId
-			}
-		});
+		dispatch(markCurrentCycleAsFinishedAction());
 	}
 
 	function createNewCycle(data: CreateCycleData) {
@@ -55,22 +51,12 @@ function CyclesContextProvider({ children }: CyclesContextProviderProps) {
 			startDate: new Date()
 		};
 
-		dispatch({
-			type: ActionTypes.ADD_NEW_CYCLE,
-			payload: {
-				newCycle
-			}
-		});
+		dispatch(addNewCycleAction(newCycle));
 		setAmountSecondsPassed(0);
 	}
 
 	function interruptCurrentCycle() {
-		dispatch({
-			type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-			payload: {
-				activeCycleId
-			}
-		});
+		dispatch(interruptCurrentCycleAction());
 	}
 
 	return (
